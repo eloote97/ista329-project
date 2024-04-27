f95f84e49cd7929474d8bb31841f9c850fbe1164
AllYouCanEatSpa <AllYouCanEatSpa@gmail.com>
2024-04-27 13:21:14 -0700

    Added 2 more controls the form

--- footer/footerHTML.js
+++ footer/footerHTML.js
@@ -6,6 +6,11 @@ $(document).ready(function() {
             <h2>Subscribe to our Newsletter</h2>
             <p id="successMessage" style="display: none;">Thank you for subscribing!</p>
             <form id="subscriptionForm" action="#">
+                <label for="nameInput">Name:</label>
+                <input type="text" name="name" id="nameInput" placeholder="Enter your name" required>
+                <label for="phoneInput">Phone Number:</label>
+                <input type="tel" name="phone" id="phoneInput" placeholder="Enter your phone number" required>
+                <label for="emailInput">Email:</label>
                 <input type="email" name="email" id="emailInput" placeholder="Enter your email" required>
                 <button type="submit">Subscribe</button>
             </form>
@@ -20,5 +25,7 @@ $(document).ready(function() {
         event.preventDefault();
         $('#successMessage').fadeIn(500).delay(2000).fadeOut(500);
         $('#emailInput').val('');
+        $('#nameInput').val('');
+        $('#phoneInput').val('');
     });
 });
