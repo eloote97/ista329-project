554c723fcbef0ed9d59cda8e4a4fcd83d879f557
tobinnickels <tobinn@email.arizona.edu>
2024-04-27 22:02:15 -0700

    update title with js

--- header/headerHTML.js
+++ header/headerHTML.js
@@ -6,14 +6,14 @@ $(document).ready(function() {
                 <div>
                     <a href="../../index.html">Restaurants</a>
                 </div>
-                <p class="title">Tucson Eats</p>
+                <p class="title">${document.title}</p>
                 <div>
                     <a href="about.html">About Us</a>
                 </div>
             </nav>
         </header>
     `;
-    
+
     $('body').prepend(headerHTML);
 });
 
